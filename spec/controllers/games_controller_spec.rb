09bad564e21e70f89228411f7eb69454e74fd429
Anikram <4nikram@gmail.com>
2018-06-01 11:15:28 +0300

    games_controller_spec added

---
+++ spec/controllers/games_controller_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+require 'support/my_spec_helper'
+
+RSpec.describe GamesController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+  let(:admin) { FactoryBot.create(:user, is_admin: true) }
+  let(:game_w_questions) { FactoryBot.create(:game_with_questions, user: user) }
+
+  context 'Anonimous users' do
+    it 'kick from #show' do
+      get :show, id: game_w_questions.id
+
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+  end
+
+  context 'User' do
+
+    before(:each) do
+      sign_in user
+    end
+
+    it 'creates game' do
+      generate_questions(60)
+
+      post :create
+
+      game = assigns(:game)
+
+      expect(game.finished?).to be_falsey
+      expect(game.user).to eq(user)
+
+      expect(response).to redirect_to game_path(game)
+      expect(flash[:notice]).to be
+    end
+
+    it '#show game' do
+      get :show, id: game_w_questions.id
+      game = assigns(:game)
+
+      expect(game.finished?).to be_falsey
+      expect(game.user).to eq(user)
+      expect(response.status).to eq 200
+
+      expect(response).to render_template('show')
+    end
+
+    it 'answered correctly' do
+      put :answer, id: game_w_questions.id, letter: game_w_questions.current_game_question.correct_answer_key
+
+      game = assigns(:game)
+
+      expect(game.finished?)
+      expect(game.current_level).to be > 0
+      expect(response).to redirect_to(game_path(game))
+    end
+  end
+end
